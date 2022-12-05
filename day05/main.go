4cd5e98c2f7d2549793a99f7e874f7642747e5ed
Nathan Do <nathan.do@alumni.virginia.edu>
2022-12-05 13:10:01 -0500

    add day 5 comment

--- day05/main.go
+++ day05/main.go
@@ -5,11 +5,8 @@ import (
 	"os"
 )
 
-// None of the alternatives I've seen in my quick peruse have really impressed me, but they do
-// seem better than my spaghetti code. I've seen uses of modulus that are interesting too.
-//
-// https://github.com/tudorpavel/advent-2022/blob/master/day05/main.go
-// https://github.com/mnml/aoc/blob/main/2022/02/1.go
+// In retrospect it might've been better to just manipulate the input as strings instead of
+// importing a stack package, but I learned how to do that so :^)
 func main() {
 	if os.Args[1] == "1" {
 		day05.Part1()
