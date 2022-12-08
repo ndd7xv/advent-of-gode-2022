4199f909c539eb9c38b903e2c01231ee5b084d82
Nathan Do <nathan.do@alumni.virginia.edu>
2022-12-08 00:51:14 -0500

    ugly/unidiomatic day 8

--- day08/lib/part1.go
+++ day08/lib/part1.go
@@ -9,11 +9,65 @@ import (
 func Part1() {
 	input, _ := os.ReadFile("day08/inputs/input1.txt")
 
-	for _, line := range strings.Split(string(input), "\n") {
-		if line == "" {
-			break
-		}
+	lines := strings.Split(string(input), "\n")
+	rows := len(lines)
+	columns := len(lines[0])
 
+	grid := make([][][]int, rows)
+	for i := 0; i < rows; i++ {
+		grid[i] = make([][]int, columns)
+		for c := 0; c < columns; c++ {
+			grid[i][c] = make([]int, 2)
+		}
 	}
-	fmt.Println("Part 1, ready to go!")
+
+	for r, line := range lines {
+		for c, ch := range line {
+			grid[r][c][0] = int(ch - '0')
+		}
+	}
+
+	visible := 0
+	for r := range lines {
+		tallestFromLeft := -1
+		tallestFromRight := -1
+		for c := range lines[0] {
+			if grid[r][c][0] > tallestFromLeft {
+				if grid[r][c][1] != 1 {
+					visible++
+					grid[r][c][1] = 1
+				}
+				tallestFromLeft = grid[r][c][0]
+			}
+			if grid[r][len(lines[0])-1-c][0] > tallestFromRight {
+				if grid[r][len(lines[0])-1-c][1] != 1 {
+					visible++
+					grid[r][len(lines[0])-1-c][1] = 1
+				}
+				tallestFromRight = grid[r][len(lines[0])-1-c][0]
+			}
+		}
+	}
+
+	for c := range lines[0] {
+		tallestFromBottom := -1
+		tallestFromTop := -1
+		for r := range lines {
+			if grid[r][c][0] > tallestFromTop {
+				if grid[r][c][1] != 1 {
+					visible++
+					grid[r][c][1] = 1
+				}
+				tallestFromTop = grid[r][c][0]
+			}
+			if grid[len(lines)-1-r][c][0] > tallestFromBottom {
+				if grid[len(lines)-1-r][c][1] != 1 {
+					visible++
+					grid[len(lines)-1-r][c][1] = 1
+				}
+				tallestFromBottom = grid[len(lines)-1-r][c][0]
+			}
+		}
+	}
+	fmt.Println(visible)
 }
--- day08/lib/part2.go
+++ day08/lib/part2.go
@@ -2,6 +2,7 @@ package day08
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strings"
 )
@@ -9,11 +10,70 @@ import (
 func Part2() {
 	input, _ := os.ReadFile("day08/inputs/input1.txt")
 
-	for _, line := range strings.Split(string(input), "\n") {
-		if line == "" {
-			break
+	lines := strings.Split(string(input), "\n")
+	rows := len(lines)
+	columns := len(lines[0])
+
+	grid := make([][]int, rows)
+	for i := 0; i < rows; i++ {
+		grid[i] = make([]int, columns)
+
+	}
+
+	for r, line := range lines {
+		for c, ch := range line {
+			grid[r][c] = int(ch - '0')
 		}
 	}
 
-	fmt.Println("Part 2, ready to go!")
+	maxScenicScore := 0.0
+	for r := range lines {
+		for c := range lines[0] {
+			maxScenicScore = math.Max(maxScenicScore, calculateScenicScore(grid, r, c))
+		}
+	}
+	fmt.Println(maxScenicScore)
+}
+
+func calculateScenicScore(grid [][]int, r int, c int) float64 {
+	score := 1
+	height := grid[r][c]
+
+	count := 0
+	for i := c - 1; i >= 0; i-- {
+		count++
+		if grid[r][i] >= height {
+			break
+		}
+	}
+	score *= count
+
+	count = 0
+	for i := c + 1; i < len(grid[0]); i++ {
+		count++
+		if grid[r][i] >= height {
+			break
+		}
+	}
+	score *= count
+
+	count = 0
+	for i := r - 1; i >= 0; i-- {
+		count++
+		if grid[i][c] >= height {
+			break
+		}
+	}
+	score *= count
+
+	count = 0
+	for i := r + 1; i < len(grid); i++ {
+		count++
+		if grid[i][c] >= height {
+			break
+		}
+	}
+	score *= count
+
+	return float64(score)
 }
