aa86bba133e2bc4f4b4e508242a7ec6a1dbda162
Nathan Do <nathan.do@alumni.virginia.edu>
2022-12-09 00:57:06 -0500

    ugly day09

--- day09/lib/part1.go
+++ day09/lib/part1.go
@@ -3,17 +3,98 @@ package day09
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
+type Coordinate struct {
+	x int
+	y int
+}
+
 func Part1() {
 	input, _ := os.ReadFile("day09/inputs/input1.txt")
+
+	tailLocations := make(map[Coordinate]bool)
+	head := Coordinate{0, 0}
+	tail := Coordinate{0, 0}
+
+	tailLocations[tail] = true
 
 	for _, line := range strings.Split(string(input), "\n") {
 		if line == "" {
 			break
 		}
-		
+
+		words := strings.Split(line, " ")
+		direction := words[0]
+		steps, _ := strconv.Atoi(words[1])
+
+		fmt.Printf("%s %s\n", direction, words[1])
+		switch direction {
+		case "U":
+			for i := 0; i < steps; i++ {
+				head = Coordinate{head.x, head.y + 1}
+				head, tail = ReadjustTail(head, tail)
+				tailLocations[tail] = true
+			}
+		case "D":
+			for i := 0; i < steps; i++ {
+				head = Coordinate{head.x, head.y - 1}
+				head, tail = ReadjustTail(head, tail)
+				tailLocations[tail] = true
+			}
+		case "L":
+			for i := 0; i < steps; i++ {
+				head = Coordinate{head.x - 1, head.y}
+				head, tail = ReadjustTail(head, tail)
+				tailLocations[tail] = true
+			}
+		case "R":
+			for i := 0; i < steps; i++ {
+				head = Coordinate{head.x + 1, head.y}
+				head, tail = ReadjustTail(head, tail)
+				tailLocations[tail] = true
+			}
+		}
 	}
-	fmt.Println("Part 1, ready to go!")
+	fmt.Println(len(tailLocations))
+}
+
+func ReadjustTail(head, tail Coordinate) (
+	Coordinate,
+	Coordinate,
+) {
+
+	fmt.Printf("Head %d %d | Tail %d %d... to", head.x, head.y, tail.x, tail.y)
+
+	dX := head.x - tail.x
+	dY := head.y - tail.y
+	if (head.x != tail.x && head.y != tail.y) && ((-2 >= dX || dX >= 2) || (-2 >= dY || dY >= 2)) {
+		if dX > 0 {
+			tail.x++
+		} else {
+			tail.x--
+		}
+		if dY > 0 {
+			tail.y++
+		} else {
+			tail.y--
+		}
+	} else if -2 >= dX || dX >= 2 {
+		if dX > 0 {
+			tail.x++
+		} else {
+			tail.x--
+		}
+	} else if -2 >= dY || dY >= 2 {
+		if dY > 0 {
+			tail.y++
+		} else {
+			tail.y--
+		}
+	}
+	fmt.Printf(" Head %d %d | Tail %d %d\n", head.x, head.y, tail.x, tail.y)
+
+	return head, tail
 }
--- day09/lib/part2.go
+++ day09/lib/part2.go
@@ -3,17 +3,64 @@ package day09
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func Part2() {
 	input, _ := os.ReadFile("day09/inputs/input1.txt")
 
+	tailLocations := make(map[Coordinate]bool)
+	rope := make([]Coordinate, 10)
+	for i := range rope {
+		rope[i] = Coordinate{0, 0}
+	}
+	tailLocations[Coordinate{0, 0}] = true
+
 	for _, line := range strings.Split(string(input), "\n") {
 		if line == "" {
 			break
 		}
-	}
 
-	fmt.Println("Part 2, ready to go!")
+		words := strings.Split(line, " ")
+		direction := words[0]
+		steps, _ := strconv.Atoi(words[1])
+
+		fmt.Printf("%s %s\n", direction, words[1])
+		switch direction {
+		case "U":
+			for i := 0; i < steps; i++ {
+				rope[0] = Coordinate{rope[0].x, rope[0].y + 1}
+				for j := 1; j < len(rope); j++ {
+					rope[j-1], rope[j] = ReadjustTail(rope[j-1], rope[j])
+				}
+				tailLocations[rope[len(rope)-1]] = true
+			}
+		case "D":
+			for i := 0; i < steps; i++ {
+				rope[0] = Coordinate{rope[0].x, rope[0].y - 1}
+				for j := 1; j < len(rope); j++ {
+					rope[j-1], rope[j] = ReadjustTail(rope[j-1], rope[j])
+				}
+				tailLocations[rope[len(rope)-1]] = true
+			}
+		case "L":
+			for i := 0; i < steps; i++ {
+				rope[0] = Coordinate{rope[0].x - 1, rope[0].y}
+				for j := 1; j < len(rope); j++ {
+					rope[j-1], rope[j] = ReadjustTail(rope[j-1], rope[j])
+				}
+				tailLocations[rope[len(rope)-1]] = true
+			}
+		case "R":
+			for i := 0; i < steps; i++ {
+				rope[0] = Coordinate{rope[0].x + 1, rope[0].y}
+				for j := 1; j < len(rope); j++ {
+					rope[j-1], rope[j] = ReadjustTail(rope[j-1], rope[j])
+				}
+				tailLocations[rope[len(rope)-1]] = true
+			}
+		}
+	}
+	fmt.Println(len(tailLocations))
 }
