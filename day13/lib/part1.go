517505b97e6f1b421a1368a3eadbcd91c65bccca
Nathan Do <nathan.do@alumni.virginia.edu>
2022-12-13 02:48:28 -0500

    day13 (not good yikes)

--- day13/lib/part1.go
+++ day13/lib/part1.go
@@ -3,17 +3,157 @@ package day13
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
+// Item is either a list of more items, or an int.
+type Item struct {
+	items []*Item
+	value *int
+}
+
+func (item Item) valueType() string {
+	if item.items != nil {
+		return "list"
+	} else if item.value != nil {
+		return "int"
+	} else {
+		return "empty"
+	}
+}
+
 func Part1() {
 	input, _ := os.ReadFile("day13/inputs/input1.txt")
+	sum := 0
+	for index, line := range strings.Split(strings.Trim(string(input), "\n"), "\n\n") {
+		pair := strings.Split(line, "\n")
 
-	for _, line := range strings.Split(string(input), "\n") {
-		if line == "" {
-			break
+		first := Parse(pair[0])
+		second := Parse(pair[1])
+
+		fmt.Printf("Comparing the %d pair\n", index)
+
+		if ro, _ := InRightOrder(*first, *second); ro {
+			sum += (index + 1)
+			fmt.Println("CORRECT")
 		}
-		
+		//fmt.Println(line)
 	}
-	fmt.Println("Part 1, ready to go!")
+
+	fmt.Println(sum)
+}
+
+func Parse(line string) *Item {
+	// Find outer layer of brackets, assumes things are properly formatted
+	var items []string
+	// fmt.Println("Parsing", line)
+	start := strings.Index(line, "[")
+	if start != -1 {
+		end := strings.LastIndex(line, "]")
+		unparsedItems := line[start+1 : end]
+		level := 0
+		start := 0
+		for i, ch := range unparsedItems {
+			if ch == '[' {
+				level++
+			} else if ch == ']' {
+				level--
+			} else if ch == ',' && level == 0 {
+				items = append(items, unparsedItems[start:i])
+				start = i + 1
+			}
+		}
+		items = append(items, unparsedItems[start:])
+	} else {
+		if strings.Contains(line, ",") {
+			items = strings.Split(line, ",")
+		} else if len(line) != 0 {
+			num, _ := strconv.Atoi(line)
+			return &Item{
+				nil,
+				&num,
+			}
+		}
+	}
+
+	// for _, item := range items {
+	// 	fmt.Println("\t", item)
+	// }
+	// Create items from the list of items
+	parsedItems := []*Item{}
+	for _, item := range items {
+		parsedItems = append(parsedItems, Parse(item))
+	}
+
+	return &Item{
+		parsedItems,
+		nil,
+	}
+}
+
+func InRightOrder(first Item, second Item) (bool, string) {
+	if first.valueType() == "empty" && first.valueType() != "empty" {
+		return true, "stop"
+	} else if first.valueType() != "empty" && first.valueType() == "empty" {
+		return false, "stop"
+	} else if first.valueType() == "int" && second.valueType() == "int" {
+		fmt.Printf("\tComparing %d and %d:\n", *first.value, *second.value)
+		if *first.value < *second.value {
+			return true, "stop"
+		} else if *first.value == *second.value {
+			return true, ""
+		} else {
+			return false, "stop"
+		}
+	} else if first.valueType() == "list" && second.valueType() == "list" {
+		fmt.Printf("Comparing %v and %v:\n", first.items, second.items)
+		for i := 0; i < len(first.items); i++ {
+			if i >= len(second.items) {
+				return false, "stop"
+			}
+			res, cont := InRightOrder(*first.items[i], *second.items[i])
+			if cont == "stop" {
+				return res, "stop"
+			}
+		}
+		if len(second.items) > len(first.items) {
+			return true, "stop"
+		} else if len(second.items) == len(first.items) {
+			return true, ""
+		}
+	} else if first.valueType() == "int" {
+		toList := Item{[]*Item{{nil, first.value}}, nil}
+		for i := 0; i < len(toList.items); i++ {
+			if i >= len(second.items) {
+				return false, "stop"
+			}
+			res, cont := InRightOrder(*toList.items[i], *second.items[i])
+			if cont == "stop" {
+				return res, "stop"
+			}
+		}
+		if len(second.items) > len(toList.items) {
+			return true, "stop"
+		} else if len(second.items) == len(toList.items) {
+			return true, ""
+		}
+	} else if second.valueType() == "int" {
+		toList := Item{[]*Item{{nil, second.value}}, nil}
+		for i := 0; i < len(toList.items); i++ {
+			if i >= len(first.items) {
+				return true, "stop"
+			}
+			res, cont := InRightOrder(*first.items[i], *toList.items[i])
+			if cont == "stop" {
+				return res, "stop"
+			}
+		}
+		if len(toList.items) > len(first.items) {
+			return false, "stop"
+		} else if len(toList.items) == len(first.items) {
+			return true, ""
+		}
+	}
+	return false, "stop"
 }
--- day13/lib/part2.go
+++ day13/lib/part2.go
@@ -3,17 +3,35 @@ package day13
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
 func Part2() {
 	input, _ := os.ReadFile("day13/inputs/input1.txt")
 
-	for _, line := range strings.Split(string(input), "\n") {
+	messages := []Item{}
+	for _, line := range strings.Split(strings.Trim(string(input), "\n"), "\n") {
 		if line == "" {
-			break
+			continue
 		}
+		messages = append(messages, *Parse(line))
 	}
 
-	fmt.Println("Part 2, ready to go!")
+	messages = append(messages, *Parse("[[2]]"))
+	messages = append(messages, *Parse("[[6]]"))
+	sort.Slice(messages, func(first, second int) bool {
+		res, _ := InRightOrder(messages[first], messages[second])
+		return res
+	})
+
+	decoderProduct := 1
+	for i, message := range messages {
+		if message.items != nil && len(message.items) == 1 {
+			if message.items[0].items != nil && len(message.items[0].items) == 1 && message.items[0].items[0].value != nil && (*message.items[0].items[0].value == 2 || *message.items[0].items[0].value == 6) {
+				decoderProduct *= (1 + i)
+			}
+		}
+	}
+	fmt.Println(decoderProduct)
 }
