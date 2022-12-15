48e069be7279491ab08d07c92d630728ea8372bd
Nathan Do <nathan.do@alumni.virginia.edu>
2022-12-15 01:54:43 -0500

    day 15

--- day15/lib/part1.go
+++ day15/lib/part1.go
@@ -2,18 +2,87 @@ package day15
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
+type Coordinate struct {
+	x, y int
+}
+
 func Part1() {
 	input, _ := os.ReadFile("day15/inputs/input1.txt")
+
+	sensors := []Coordinate{}
+	beacons := []Coordinate{}
+	distances := []int{}
 
 	for _, line := range strings.Split(string(input), "\n") {
 		if line == "" {
 			break
 		}
-		
+		sensor, beacon := ParseLine(line)
+
+		sensors = append(sensors, sensor)
+		beacons = append(beacons, beacon)
+		distances = append(distances, ManhattanDistance(sensor, beacon))
+
+		// fmt.Printf("Sensor: (%d, %d) | Beacon: (%d, %d)\n", sensor.x, sensor.y, beacon.x, beacon.y)
 	}
-	fmt.Println("Part 1, ready to go!")
+
+	invalidX := make(map[int]bool)
+
+	for i, sensor := range sensors {
+		dY := math.Abs(float64(sensor.y) - 2000000)
+		dX := distances[i] - int(dY)
+		// fmt.Printf("%f\n", dY)
+		if dX >= 0 {
+			for j := -dX + sensor.x; j <= dX+sensor.x; j++ {
+				invalidX[int(j)] = true
+				for _, beacon := range beacons {
+					if beacon.x == j && beacon.y == 2000000 {
+						delete(invalidX, int(j))
+					}
+				}
+			}
+		}
+	}
+
+	// keys := []int{}
+	// for k := range invalidX {
+	// 	keys = append(keys, k)
+	// }
+	// sort.Ints(keys)
+	// fmt.Printf("%v", keys)
+	fmt.Println(len(invalidX))
+}
+
+func ManhattanDistance(a, b Coordinate) int {
+	return int(math.Abs(float64(a.x)-float64(b.x)) + math.Abs(float64(a.y)-float64(b.y)))
+}
+
+func ParseLine(s string) (Coordinate, Coordinate) {
+	split := strings.Split(s, " closest ")
+
+	var x, y int
+	for _, word := range strings.Split(split[0], " ") {
+		if strings.HasPrefix(word, "x=") {
+			x, _ = strconv.Atoi(word[2 : len(word)-1])
+		} else if strings.HasPrefix(word, "y=") {
+			y, _ = strconv.Atoi(word[2 : len(word)-1])
+		}
+	}
+	sensor := Coordinate{x, y}
+
+	for _, word := range strings.Split(split[1], " ") {
+		if strings.HasPrefix(word, "x=") {
+			x, _ = strconv.Atoi(word[2 : len(word)-1])
+		} else if strings.HasPrefix(word, "y=") {
+			y, _ = strconv.Atoi(word[2:])
+		}
+	}
+	beacon := Coordinate{x, y}
+	return sensor, beacon
 }
--- day15/lib/part2.go
+++ day15/lib/part2.go
@@ -2,6 +2,7 @@ package day15
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strings"
 )
@@ -9,11 +10,46 @@ import (
 func Part2() {
 	input, _ := os.ReadFile("day15/inputs/input1.txt")
 
+	sensors := []Coordinate{}
+	// beacons := []Coordinate{}
+	distances := []int{}
+
 	for _, line := range strings.Split(string(input), "\n") {
 		if line == "" {
 			break
 		}
+		sensor, beacon := ParseLine(line)
+
+		sensors = append(sensors, sensor)
+		// beacons = append(beacons, beacon)
+		distances = append(distances, ManhattanDistance(sensor, beacon))
+
+		// fmt.Printf("Sensor: (%d, %d) | Beacon: (%d, %d)\n", sensor.x, sensor.y, beacon.x, beacon.y)
 	}
 
-	fmt.Println("Part 2, ready to go!")
+out:
+	for x := 0; x <= 4000000; x++ {
+		for y := 0; y <= 4000000; y++ {
+			// Apparently running this code in my terminal w/o the if check would eventually stop
+			// printing to stdout?
+			// if x > 4000000 {
+			// 	fmt.Printf("Testing (%d, %d)\n", x, y)
+			// }
+			invalid := false
+			for i, sensor := range sensors {
+				if ManhattanDistance(sensor, Coordinate{x, y}) <= distances[i] {
+					dX := int(math.Abs(float64(sensor.x - x)))
+					dY := y - sensor.y
+					y += distances[i] - dX - dY
+					invalid = true
+					break
+				}
+			}
+			if !invalid {
+				fmt.Printf("(%d, %d): %d\n", x, y, x*4000000+y)
+				break out
+			}
+		}
+	}
+
 }
